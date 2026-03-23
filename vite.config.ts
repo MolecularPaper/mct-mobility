90e3f214dc968fce11a807a90f12812766faaeb4
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2026-03-23 09:42:30 +0000

    build: vite 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), reactClickToComponent(), tailwindcss()],
+  server: {
+    host: true, // 외부 접근을 허용하기 위해 필요
+    strictPort: true,
+    port: 5173,
+    hmr: {
+      clientPort: 443, // Codespaces 환경에서는 HTTPS 포트(443)를 통해 WebSocket 통신을 해야함.
+    },
+  },
 });
