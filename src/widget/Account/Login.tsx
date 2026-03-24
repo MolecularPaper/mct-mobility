fb2550a39ec17f62f5a0e558d05cea7ba35734fc
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2026-03-24 04:54:00 +0000

    style: 계정 관리 페이지 요소 추가

--- src/widget/Account/Login.tsx
+++ src/widget/Account/Login.tsx
@@ -1,15 +1,38 @@
 import { AccountChildProps } from "./Account";
+
+import { useUser } from "@/states/user";
+
+import Input from "@/components/Input";
 import Button from "@/components/Button";
 
 export default function Login({ setActiveLoginPage }: AccountChildProps) {
+  const login = useUser((state) => state.login);
+
+  function RequestLogin() {
+    console.log("로그인 요청");
+    login("abc", "임시");
+  }
+  function ResponseLogin() {}
+
   return (
-    <div className="w-1/2 h-3/4 bg-white flex flex-col">
-      <p className="self-center text-4xl mt-4">로그인</p>
-      <Button
-        className="cursor-pointer self-end m-2 mt-auto mb-1"
+    <div className="w-96 h-fit min-h-75 bg-white flex flex-col p-8">
+      <p className="self-center text-4xl">로그인</p>
+      <Input
+        className="mt-8 text-2xl"
+        inputProps={{ className: "flex-1", placeholder: "아이디" }}
+      />
+      <Input
+        className="mt-6 text-2xl"
+        inputProps={{ className: "flex-1", placeholder: "비밀번호" }}
+      />
+      <Button className="mt-6" onClick={RequestLogin}>
+        로그인
+      </Button>
+      <button
+        className="cursor-pointer self-center mt-4 text-gray-500"
         onClick={() => setActiveLoginPage(false)}>
         가입하기
-      </Button>
+      </button>
     </div>
   );
 }
--- src/widget/Account/Register.tsx
+++ src/widget/Account/Register.tsx
@@ -1,15 +1,34 @@
 import { AccountChildProps } from "./Account";
+import Input from "@/components/Input";
 import Button from "@/components/Button";
 
 export default function Register({ setActiveLoginPage }: AccountChildProps) {
+  function RequestRegister() {}
+  function ResponseRegister() {}
+
   return (
-    <div className="w-1/2 h-3/4 bg-white flex flex-col">
-      <p className="self-center text-4xl mt-4">가입하기</p>
-      <Button
-        className="cursor-pointer self-end m-2 mt-auto mb-1"
-        onClick={() => setActiveLoginPage(true)}>
-        로그인 하기
+    <div className="w-96 h-fit min-h-75 bg-white flex flex-col p-8">
+      <p className="self-center text-4xl">가입</p>
+      <Input
+        className="mt-8 text-2xl"
+        inputProps={{ className: "flex-1", placeholder: "아이디" }}
+      />
+      <Input
+        className="mt-6 text-2xl"
+        inputProps={{ className: "flex-1", placeholder: "비밀번호" }}
+      />
+      <Input
+        className="mt-6 text-2xl"
+        inputProps={{ className: "flex-1", placeholder: "비밀번호 확인" }}
+      />
+      <Button className="mt-6" onClick={RequestRegister}>
+        가입하기
       </Button>
+      <button
+        className="cursor-pointer self-center mt-4 text-gray-500"
+        onClick={() => setActiveLoginPage(true)}>
+        로그인
+      </button>
     </div>
   );
 }
