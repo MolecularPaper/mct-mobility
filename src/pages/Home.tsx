a03ff683b7752b80b0f5fd888f69ce8434c943e9
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2026-03-26 05:56:05 +0000

    style(Home.tsx): 전체적인 디자인 및 이미지 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
 import baner from "@/assets/baner.gif";
-import road from "@/assets/road.svg";
-import passengerPageImg from "@/assets/passenger_page.png";
+
+import driver from "@/assets/driver.webp";
+import passenger from "@/assets/passenger.webp";
+import taxi from "@/assets/taxi.webp";
 
 /**
  * 홈페이지 컴포넌트
@@ -22,37 +24,48 @@ export default function Home() {
           />
         </div>
         {/* 하단 분기 영역 */}
-        <div className="flex flex-1">
-          <Link
-            to="/driver"
-            className="relative flex flex-1 items-center justify-center no-underline text-neutral-900 cursor-pointer transition-[filter] duration-150 hover:brightness-[0.93]">
-            <div className="relative flex flex-1 items-center justify-center w-full self-stretch">
+        <div className="flex flex-1 no-underline text-white text-[20px] font-bold">
+          <div className="flex flex-1 flex-col items-center justify-center">
+            <Link
+              to="/driver"
+              className="relative flex flex-1 flex-col w-full items-center justify-center cursor-pointer">
               <img
-                src={road}
+                src={driver}
                 alt="운전자"
-                className="absolute inset-0 w-full h-full object-cover"
+                className="absolute w-full h-full object-cover object-bottom-left"
               />
-              <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[clamp(1.3rem,3.5vw,2rem)] font-black text-center leading-snug m-0 pointer-events-none whitespace-nowrap">
+              <p className="z-2 drop-shadow-md">
                 함께 타고 가요
                 <br />
               </p>
-            </div>
-          </Link>
-
-          <Link
-            to="/passenger"
-            className="relative flex flex-1 items-center justify-center no-underline text-neutral-900 cursor-pointer bg-[#e84a2c] transition-[filter] duration-150 hover:brightness-[0.93]">
-            <div className="relative flex flex-1 items-center justify-center w-full p-6 self-stretch">
+            </Link>
+            <Link
+              to="/passenger"
+              className="relative flex flex-1 flex-col w-full items-center justify-center cursor-pointer">
               <img
-                src={passengerPageImg}
+                src={passenger}
                 alt="승객"
-                className="max-w-[80%] max-h-full object-contain"
+                className="absolute w-full h-full object-cover"
               />
-              <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[clamp(1.3rem,3.5vw,2rem)] font-black text-center leading-snug m-0 pointer-events-none whitespace-nowrap">
+              <p className="z-2 drop-shadow-md">
                 이쪽으로 가요
                 <br />
               </p>
-            </div>
+            </Link>
+          </div>
+
+          <Link
+            to="/taxi"
+            className="relative flex flex-1 box-border items-center justify-center cursor-pointer">
+            <img
+              src={taxi}
+              alt="택시"
+              className="absolute w-full h-full object-cover object-right"
+            />
+            <p className="z-2 drop-shadow-md">
+              택시
+              <br />
+            </p>
           </Link>
         </div>
       </div>
