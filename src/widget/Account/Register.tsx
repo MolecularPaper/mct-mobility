a07eaa0cd0ef98cb570f4d59bc305c10f7689c9a
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2026-03-26 23:57:13 +0000

    feat: 가입 위젯이 승인코드 입력 추가

--- src/widget/Account/Register.tsx
+++ src/widget/Account/Register.tsx
@@ -33,6 +33,14 @@ export default function Register({ setActiveLoginPage }: AccountChildProps) {
           placeholder: "비밀번호 확인",
         }}
       />
+      <Input
+        className="mt-6 text-[15px]"
+        inputProps={{
+          className:
+            "flex-1 rounded-lg bg-gray-100 px-3 py-2.5 outline-none focus:ring-2 focus:ring-blue-400",
+          placeholder: "승인코드",
+        }}
+      />
       <Button
         className="mt-6 text-[14px] rounded-xl bg-blue-400 border-none font-bold text-white hover:bg-blue-500 focus:bg-blue-500"
         onClick={RequestRegister}>
