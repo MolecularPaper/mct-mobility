63643408f96ab3e7b87efee96cb706f5f7de8ffb
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2026-03-27 05:58:31 +0000

    feat: 환경변수에서 DB명을 관리할 수 있도록 변경

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,8 +2,8 @@ import { MongoClient, Db } from "mongodb";
 import process from "process";
 
 const uri = import.meta.env.VITE_DB_URI || "";
+const dbName = import.meta.env.VITE_DB_NAME || "";
 const client = new MongoClient(uri);
-const databaseName = "mct_mobility";
 
 let db: Db;
 
@@ -11,7 +11,7 @@ export async function connectDB() {
   try {
     await client.connect();
     console.log("MongoDB Atlas 연결");
-    db = client.db(databaseName); // 사용할 데이터베이스 이름
+    db = client.db(dbName); // 사용할 데이터베이스 이름
     return db;
   } catch (error) {
     console.error("DB 연결 에러: ", error);
