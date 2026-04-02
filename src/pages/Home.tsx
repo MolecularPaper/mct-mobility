cf6c4ac8d6b3ad8b821ce81e2d0f52c2f88237de
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2026-04-02 04:36:35 +0000

    style(home): 사이트 제목에 HY헤드라인, 부제목에 HY중고딕 폰트 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,10 +21,12 @@ export default function Home() {
       <div className="flex flex-col h-screen overflow-y-auto">
         <div className="w-full p-4 flex justify-center items-center bg-blue-300">
           <div className="flex-1">
-            <span className="text-3xl font-bold text-left h-min min-h-min">
+            <span
+              className="text-3xl font-bold text-left h-min min-h-min"
+              style={{ fontFamily: "HYHeadLine" }}>
               상승 GO!
             </span>
-            <div className="text-1xl">
+            <div className="text-1xl" style={{ fontFamily: "HYJungGothic" }}>
               <p>{"빠르고, 함께하고, 비용도 아끼는"}</p>
               <p>{"스마트한 상승 인(人)"}</p>
             </div>
